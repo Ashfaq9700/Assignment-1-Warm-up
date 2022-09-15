cc32ee235f02b20784d01ca797829bea1b42f8cd
Ashfaq9700 <113584553+Ashfaq9700@users.noreply.github.com>
2022-09-15 15:39:47 +0530

    Add files via upload

---
+++ Warm-up.py
@@ -0,0 +1,42 @@
+#1 Write a command to get the Python version you are using.
+# Solution : python --version
+
+#2 Write a python script to print Hello Python on the screen directly in the command line.
+# Solution : print("Hello Python")
+
+#3 Write a python script and store it in a file and execute the file from the command line.
+# Solution : python warm-up.py
+
+#4 Write a python script to print Hello Python on the screen using visual studio code.
+# Solution : print("Hello Python")
+
+#5 Write a python script to print Hello on the first line and Python on the second line
+# Solution : print('Hello \nPython')
+
+#6 Write a python script to print “MySirG” on the screen
+# Solution : print('"MySirG"') OR print('\"MySirG"')
+
+#7 Write a python script to print “Teacher’s Day” on the screen
+# Solution : print('"Teacher’s Day"')
+
+#8 Write a python script to print the value of a variable. Variable contains your name as data.
+""" Solution : 
+userName = "Shaik Ashfaq"
+print(userName)"""
+
+#9 Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+""" Solution : 
+name = input("Enter Your Name : ")
+age = int(input("Enter Your Age : "))
+qualification = input("Enter Your Qualification : ")
+experience = int(input("Enter Your Number of Experience : "))"""
+
+#10 Write a python script to print values of three variables, each in a new line. Variable contains integer values
+""" Solution : 
+num_1 = 10
+num_2 = 20
+num_3 = 30
+   
+print(num_1,num_2,num_3,sep='\n')"""
+
+
